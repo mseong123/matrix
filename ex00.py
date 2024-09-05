b5a791f704f7a659b0870cccc561eff92c339ed9
Meng Seong Lee <melee@u91z03s03.42kl.edu.my>
2024-09-05 10:36:29 +0800

    exercise1 done

--- ex00.py
+++ ex00.py
@@ -78,7 +78,73 @@ def main():
             [21.,21.],
             [21.,21.]
         ])
-        # evalsheet pdf (deduction)
+        # evalsheet pdf (subtraction)
+        eval_a_ded:Vector[float] = Vector([0., 0.])
+        eval_b_ded:Vector[float] = Vector([0., 0.])
+        eval_c_ded:Vector[float] = Vector([1., 0.])
+        eval_d_ded:Vector[float] = Vector([0., 1.])
+        eval_e_ded:Vector[float] = Vector([1., 1.])
+        eval_f_ded:Vector[float] = Vector([1., 1.])
+        eval_g_ded:Vector[float] = Vector([21., 21.])
+        eval_h_ded:Vector[float] = Vector([21., 21.])
+        eval_i_ded:Vector[float] = Vector([-21., 21.])
+        eval_j_ded:Vector[float] = Vector([21., -21.])
+        eval_k_ded:Vector[float] = Vector([0., 1., 2., 3., 4., 5., 6., 7., 8., 9.])
+        eval_l_ded:Vector[float] = Vector([9., 8., 7., 6., 5., 4., 3., 2., 1., 0.])
+        eval_m_ded:Matrix[float] = Matrix([
+            [0.,0.],
+            [0.,0.]
+        ])
+        eval_n_ded:Matrix[float] = Matrix([
+            [0.,0.],
+            [0.,0.]
+        ])
+        eval_o_ded:Matrix[float] = Matrix([
+            [1.,0.],
+            [0.,1.]
+        ])
+        eval_p_ded:Matrix[float] = Matrix([
+            [0.,0.],
+            [0.,0.]
+        ])
+        eval_q_ded:Matrix[float] = Matrix([
+            [1.,1.],
+            [1.,1.]
+        ])
+        eval_r_ded:Matrix[float] = Matrix([
+            [1.,1.],
+            [1.,1.]
+        ])
+        eval_s_ded:Matrix[float] = Matrix([
+            [21.,21.],
+            [21.,21.]
+        ])
+        eval_t_ded:Matrix[float] = Matrix([
+            [21.,21.],
+            [21.,21.]
+        ])
+        # evalsheet pdf (multiply)
+        eval_a_scl:Vector[float] = Vector([0., 0.])
+        eval_b_scl:Vector[float] = Vector([1., 0.])
+        eval_c_scl:Vector[float] = Vector([1., 1.])
+        eval_d_scl:Vector[float] = Vector([21., 21.])
+        eval_e_scl:Vector[float] = Vector([42., 42.])
+        eval_f_scl:Matrix[float] = Matrix([
+            [0.,0.],
+            [0.,0.]
+        ])
+        eval_g_scl:Matrix[float] = Matrix([
+            [1.,0.],
+            [0.,1.]
+        ])
+        eval_h_scl:Matrix[float] = Matrix([
+            [1.,2.],
+            [3.,4.]
+        ])
+        eval_i_scl:Matrix[float] = Matrix([
+            [21.,21.],
+            [21.,21.]
+        ])
     except (TypeError, ValueError) as e:
         print(f"Error occured: {e}")
         return
@@ -137,8 +203,68 @@ def main():
         eval_s_add.add(eval_t_add)
         eval_s_add.print_matrix()
         print("")
-        print("Vector operations (deductions)")
-        
+        print("Vector operations (subtractions)")
+        eval_a_ded.sub(eval_b_ded)
+        eval_a_ded.print_vector()
+        print("")
+        eval_c_ded.sub(eval_d_ded)
+        eval_c_ded.print_vector()
+        print("")
+        eval_e_ded.sub(eval_f_ded)
+        eval_e_ded.print_vector()
+        print("")
+        eval_g_ded.sub(eval_h_ded)
+        eval_g_ded.print_vector()
+        print("")
+        eval_i_ded.sub(eval_j_ded)
+        eval_i_ded.print_vector()
+        print("")
+        eval_k_ded.sub(eval_l_ded)
+        eval_k_ded.print_vector()
+        print("")
+        print("Matrix operations (subtractions)")
+        eval_m_ded.sub(eval_n_ded)
+        eval_m_ded.print_matrix()
+        print("")
+        eval_o_ded.sub(eval_p_ded)
+        eval_o_ded.print_matrix()
+        print("")
+        eval_q_ded.sub(eval_r_ded)
+        eval_q_ded.print_matrix()
+        print("")
+        eval_s_ded.sub(eval_t_ded)
+        eval_s_ded.print_matrix()
+        print("")
+        print("Vector operations (multiply)")
+        eval_a_scl.scl(1.)
+        eval_a_scl.print_vector()
+        print("")
+        eval_b_scl.scl(1.)
+        eval_b_scl.print_vector()
+        print("")
+        eval_c_scl.scl(2.)
+        eval_c_scl.print_vector()
+        print("")
+        eval_d_scl.scl(2.)
+        eval_d_scl.print_vector()
+        print("")
+        eval_e_scl.scl(0.5)
+        eval_e_scl.print_vector()
+        print("")
+        print("Matrix operations (multiply)")
+        eval_f_scl.scl(0.)
+        eval_f_scl.print_matrix()
+        print("")
+        eval_g_scl.scl(1.)
+        eval_g_scl.print_matrix()
+        print("")
+        eval_h_scl.scl(2.)
+        eval_h_scl.print_matrix()
+        print("")
+        eval_i_scl.scl(0.5)
+        eval_i_scl.print_matrix()
+        print("")
+
 
     except (TypeError, ValueError) as e:
         print(f"Error occured: {e}")
