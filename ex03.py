f4613e849408fbd69b11e90e7650b122c1f956da
Meng Seong Lee <melee@u91z03s03.42kl.edu.my>
2024-09-05 12:08:54 +0800

    ex03.py done

--- ex03.py
+++ ex03.py
@@ -6,19 +6,42 @@ def main():
     '''Test case for exercise 03'''
     print("Results for exercise 03")
     try:
+        # pdf test
         u:Vector[float] = Vector([0., 0.])
         v:Vector[float] = Vector([1., 1.])
         a:Vector[float] = Vector([1., 1.])
         b:Vector[float] = Vector([1., 1.])
         c:Vector[float] = Vector([-1., 6.])
         d:Vector[float] = Vector([3., 2.])
+        # eval test
+        f:Vector[float] = Vector([0., 0.])
+        g:Vector[float] = Vector([0., 0.])
+        h:Vector[float] = Vector([1., 0.])
+        i:Vector[float] = Vector([0., 0.])
+        j:Vector[float] = Vector([1., 0.])
+        k:Vector[float] = Vector([1., 0.])
+        l:Vector[float] = Vector([1., 0.])
+        m:Vector[float] = Vector([0., 1.])
+        n:Vector[float] = Vector([1., 1.])
+        o:Vector[float] = Vector([1., 1.])
+        p:Vector[float] = Vector([4., 2.])
+        q:Vector[float] = Vector([2., 1.])
     except (TypeError, ValueError) as e:
         print(f"Error occured: {e}")
         return
     try:
+        print("pdf test")
         print(u.dot(v))
         print(a.dot(b))
         print(c.dot(d))
+        print("")
+        print("eval test")
+        print(f.dot(g))
+        print(h.dot(i))
+        print(j.dot(k))
+        print(l.dot(m))
+        print(n.dot(o))
+        print(p.dot(q))
     except (TypeError, ValueError) as e:
         print(f"Error occured: {e}")
         return
